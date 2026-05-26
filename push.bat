269b7262b2226c7df7f7a1bd7a9070e26ff7ed99
yousef alhameadi <yousefalhameadi@gmail.com>
2026-05-27 00:25:37 +0300

    Added a new problem solution

--- push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Syncing with GitHub...
-git pull origin main
-echo Uploading new code...
-git add .
-git commit -m "Added a new problem solution"
-git push origin main
-echo Successfully uploaded!
-pause
